ba95b31a50a30162eeb3cf77ccb9b762ede9cef4
Krishna Annam <annamkishore@gmail.com>
2023-09-05 00:50:20 +0530

    async/await

--- app/redux/page.js
+++ app/redux/page.js
@@ -1,6 +1,6 @@
 'use client'
 
-// libraries reduxjs/toolkit, react-redux,
+// libraries-------- reduxjs/toolkit, react-redux,
 import {createSlice, configureStore} from "@reduxjs/toolkit";
 import {Provider, useDispatch, useSelector} from "react-redux";
 
@@ -10,7 +10,8 @@ const counterSlice = createSlice({
     initialState: 0,
     reducers: {
         increment: state => state + 1,
-        decrement: state => state - 1
+        decrement: state => state - 1,
+        incrementByVal: (state, action) => state + action.payload
     }
 })
 // Step #2---------- create store
@@ -20,7 +21,18 @@ let store = configureStore({
     }
 })
 // Step #3---------- expose actions
-let {increment, decrement} = counterSlice.actions
+let {
+    increment,
+    decrement,
+    incrementByVal
+} = counterSlice.actions
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+const incrementAsync = val => async dispatch => {
+    await timeout(1000)
+    dispatch(incrementByVal(val))
+}
 
 // Step #4---------- wrap using Provider passing created store
 export default function App() {
@@ -37,7 +49,7 @@ function Hello() {
 
     return <>
         <h1 style={{margin: "2rem"}}>Hello {count}</h1>
-        <button onClick={()=>dispatch(increment())}>Increment</button>
-        <button onClick={()=>dispatch(decrement())}>Decrement</button>
+        <button onClick={() => dispatch(incrementAsync(2))}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
     </>
 }
