34eaf9f336130e5964fa04309e0f609fb38444d5
Krishna Annam <annamkishore@gmail.com>
2024-09-10 00:00:53 +0530

    rxjs ver 6 poc

--- app/9-rxjs/hello-ver6.js
+++ app/9-rxjs/hello-ver6.js
@@ -1,6 +1,7 @@
-import {of, Observable} from "rxjs";
+import {of, Observable, Subject} from "rxjs";
 import {filter} from 'rxjs/operators/index.js';
 import {map} from 'rxjs/operators/index.js';
+import {tap} from 'rxjs/operators/index.js';
 
 // -------------------------------------------------
 //      observable means -------------- Producer
@@ -9,7 +10,7 @@ const observable = new Observable(observer => {
     // generate some events
     observer.next(1)
     observer.next(2)
-    // throw new Error("err..")
+    throw new Error("err..")
     observer.next(3)
     setTimeout(() => {
         observer.next(4)
@@ -38,8 +39,9 @@ function test1() {
 function test2() {
     let sourceObservable = of(11, 22, 33)
     sourceObservable.pipe(
-        filter(item => item % 2),
-        map(item => item + 1)
+        filter(item => item % 2),       // filters the data
+        map(item => item + 1),          // transforms the data
+        tap(console.log)                // taps every next call but not modify
     ).subscribe(observer)
 }
 
@@ -52,10 +54,19 @@ function test3() {
     }, 2000)
 }
 
+function test4() {
+    let subject = new Subject()
+    subject.subscribe(console.log)
+    subject.next(33)
+    subject.next(44)
+    subject.complete()
+}
+
 function main() {
     // test1()
     // test2()
-    test3()
+    // test3()
+    test4()
 }
 
 main()
